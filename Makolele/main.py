b796ee4c9e56cd84251bd29f9cbf102a7936faad
Makolele <107346621+Makolele@users.noreply.github.com>
2022-09-19 18:13:34 +0200

    Update main.py

--- Makolele/main.py
+++ Makolele/main.py
@@ -1,5 +1,3 @@
-Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 helmet = 0
 rifle = 0
 medkit = 0
